d12892c9471ea1236dde67eaff7c79b1d9cf03a5
SymmetricChaos <ajfraebel@gmail.com>
2021-01-05 08:05:18 -0500

    euler 18

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ mod euler_14;
 mod euler_15;
 mod euler_16;
 mod euler_17;
+mod euler_18;
 
 fn main() {
     println!("\nWelcome to My Project Euler Rust Project!");
@@ -28,23 +29,24 @@ fn main() {
         io::stdin().read_line(&mut val).expect("Failed to read line");
 
         match val.trim()  {
-            "1" => println!("Euler  1: {}",euler_1::euler1()),
-            "2" => println!("Euler  2: {}",euler_2::euler2()),
-            "3" => println!("Euler  3: {}",euler_3::euler3()),
-            "4" => println!("Euler  4: {}",euler_4::euler4()),
-            "5" => println!("Euler  5: {}",euler_5::euler5()),
-            "6" => println!("Euler  6: {}",euler_6::euler6()),
-            "7" => println!("Euler  7: {}",euler_7::euler7()),
-            "8" => println!("Euler  8: {}",euler_8::euler8()),
-            "9" => println!("Euler  9: {}",euler_9::euler9()),
-            "10" => println!("Euler 10: {}",euler_10::euler10()),
-            "11" => println!("Euler 11: {}",euler_11::euler11()),
-            "12" => println!("Euler 12: {}",euler_12::euler12()),
-            "13" => println!("Euler 13: {}",euler_13::euler13()),
-            "14" => println!("Euler 14: {}",euler_14::euler14()),
-            "15" => println!("Euler 15: {}",euler_15::euler15()),
-            "16" => println!("Euler 16: {}",euler_16::euler16()),
-            "17" => println!("Euler 17: {}",euler_17::euler17()),
+            "1" => println!("\nEuler  1: {}",euler_1::euler1()),
+            "2" => println!("\nEuler  2: {}",euler_2::euler2()),
+            "3" => println!("\nEuler  3: {}",euler_3::euler3()),
+            "4" => println!("\nEuler  4: {}",euler_4::euler4()),
+            "5" => println!("\nEuler  5: {}",euler_5::euler5()),
+            "6" => println!("\nEuler  6: {}",euler_6::euler6()),
+            "7" => println!("\nEuler  7: {}",euler_7::euler7()),
+            "8" => println!("\nEuler  8: {}",euler_8::euler8()),
+            "9" => println!("\nEuler  9: {}",euler_9::euler9()),
+            "10" => println!("\nEuler 10: {}",euler_10::euler10()),
+            "11" => println!("\nEuler 11: {}",euler_11::euler11()),
+            "12" => println!("\nEuler 12: {}",euler_12::euler12()),
+            "13" => println!("\nEuler 13: {}",euler_13::euler13()),
+            "14" => println!("\nEuler 14: {}",euler_14::euler14()),
+            "15" => println!("\nEuler 15: {}",euler_15::euler15()),
+            "16" => println!("\nEuler 16: {}",euler_16::euler16()),
+            "17" => println!("\nEuler 17: {}",euler_17::euler17()),
+            "18" => println!("\nEuler 18: {}",euler_18::euler18()),
             "quit" => break,
             _ => println!("Haven't gotten to that one yet, sorry.")
         }
