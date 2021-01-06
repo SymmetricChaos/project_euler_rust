316e8b78132568c6ca72896adf8ae223eab40188
SymmetricChaos <ajfraebel@gmail.com>
2021-01-06 17:55:27 -0500

    euler_24

---
+++ src/euler_24.rs
@@ -0,0 +1,29 @@
+// What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+
+fn factoradic(n: u64) -> Vec<u64> {
+    let mut n = n;
+    let mut f = 1;
+    let mut v = Vec::new();
+    loop {
+        if n == 0 {
+            break;
+        }
+        v.push(n%f as u64);
+        n /= f;
+        f += 1
+    };
+    return v;
+}
+
+pub fn euler24() -> u64 {
+    let mut v = factoradic(999_999);
+    v.reverse();
+    let mut elems = vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let mut out = 0;
+    let mut ctr = 10;
+    for pos in v {
+        ctr -= 1;
+        out += elems.remove(pos as usize) * 10u64.pow(ctr);
+    }
+    return out;
+}
--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,7 @@ mod euler_20;
 mod euler_21;
 mod euler_22;
 mod euler_23;
+mod euler_24;
 
 fn main() {
     println!("\nWelcome to My Project Euler Rust Project!");
@@ -57,6 +58,7 @@ fn main() {
             "21" => println!("\nEuler 21: {}",euler_21::euler21()),
             "22" => println!("\nEuler 22: {}",euler_22::euler22()),
             "23" => println!("\nEuler 23: {}",euler_23::euler23()),
+            "24" => println!("\nEuler 24: {}",euler_24::euler24()),
             "quit" => break,
             _ => println!("Haven't gotten to that one yet, sorry.")
         }
