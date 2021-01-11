15051d05b3309aba82f2dae09f70084cab7c4d04
SymmetricChaos <ajfraebel@gmail.com>
2021-01-11 13:59:18 -0500

    semiworking euler_32

--- src/euler_32.rs
+++ src/euler_32.rs
@@ -10,26 +10,104 @@ Likewise the smallest product pair of three digit numbers allowed is 135*246 = 3
 has too many digits to be pandigital.
 
 Thus only products of a two digit and a three digit number can possibly work. This means
-there are only about 72*35 = 2520 posibilities to check, no clever optimitizted needed.
+there are less than 72*900 = 64800 posibilities to check, no clever optimitizted needed.
 */
 
-fn all_valid_two_digit() -> Vec<u64> {
+// Get all the 2-digit numbers allowed
+fn all_valid_two_digit() -> Vec<[u32; 2]> {
     let digits = [1,2,3,4,5,6,7,8,9];
     let mut out = Vec::new();
     for a in digits.iter() {
         for b in digits.iter() {
             if a != b {
-                out.push(a*10+b);
+                out.push([*a,*b]);
             }
         }
     }
     return out;
 }
 
-pub fn euler32() -> u64 {
-    let two_digit = all_valid_two_digit();
-    for t in two_digit {
-        println!("{}",t);
+// Get all the 3-digit numbers that pair with a given 2-digit number
+fn three_digit_pair(n: [u32; 2]) -> Vec<[u32; 3]> {
+    let mut digits = vec![1,2,3,4,5,6,7,8,9];
+    if n[0] > n[1] {
+        digits.remove(n[0] as usize - 1);
+        digits.remove(n[1] as usize - 1);
+    } else {
+        digits.remove(n[1] as usize - 1);
+        digits.remove(n[0] as usize - 1);
     }
-    return 0u64;
+    let mut out = Vec::new();
+    for a in digits.iter() {
+        for b in digits.iter() {
+            for c in digits.iter() {
+                if a != b && b != c && c != a{
+                    out.push([*a,*b,*c]);
+                }
+            }
+        }
+    }
+    return out;
+}
+
+fn int_to_digits(n: u64) -> [u32; 4] {
+    let mut digits = [0u32; 4];
+    let mut num = n;
+    let mut ctr = 4;
+    while num != 0 {
+        ctr -= 1;
+        let q = num/10;
+        let r = num%10;
+        digits[ctr] = r as u32;
+        num = q;
+    }
+    return digits;
+}
+
+pub fn euler32() -> u64 {
+    let mut out = 0;
+    let two_digit = all_valid_two_digit();
+    let mut prods = Vec::new();
+
+    let ds = "[1, 2, 3, 4, 5, 6, 7, 8, 9]";
+
+    for t in two_digit.iter() {
+        let n = (10*t[0] + t[1]) as u64;
+
+        let complement = three_digit_pair(*t);
+
+        for c in complement.iter() {
+            let m = (100*c[0] + 10*c[1] + c[2]) as u64;
+
+            if n*m < 1000 || n*m > 9999 {
+                continue
+            }
+            let p = int_to_digits(n*m);
+
+            let mut v = Vec::new();
+
+            for i in t.iter() {
+                v.push(i);
+            }
+            for i in c.iter() {
+                v.push(i);
+            }
+            for i in p.iter() {
+                v.push(i);
+            }
+
+            v.sort();
+
+            let vs = format!("{:?}",v);
+            if vs == ds {
+                if !prods.contains(&(m*n)) {
+                    out += m*n;
+                    prods.push(m*n);
+                }
+                println!("{} * {} = {}", n,m,n*m);
+            }
+
+        }
+    }
+    return out;
 }
