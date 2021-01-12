238d24da0e9d95e51707eab43936de8036d17d1a
SymmetricChaos <ajfraebel@gmail.com>
2021-01-11 21:02:46 -0500

    Update euler_32.rs

--- src/euler_32.rs
+++ src/euler_32.rs
@@ -16,13 +16,13 @@ the possibiliites enough that no special tricks are needed.
 */
 
 // Get all the 2-digit numbers allowed
-fn all_valid_two_digit() -> Vec<[u32; 2]> {
+fn all_valid_two_digit() -> Vec<Vec<u64>> {
     let digits = [1,2,3,4,5,6,7,8,9];
     let mut out = Vec::new();
     for a in digits.iter() {
         for b in digits.iter() {
             if a != b {
-                out.push([*a,*b]);
+                out.push(vec![*a,*b]);
             }
         }
     }
@@ -30,7 +30,7 @@ fn all_valid_two_digit() -> Vec<[u32; 2]> {
 }
 
 // Get all the 3-digit numbers that pair with a given 2-digit number
-fn three_digit_pair(n: [u32; 2]) -> Vec<[u32; 3]> {
+fn three_digit_pair(n: Vec<u64>) -> Vec<Vec<u64>> {
     let mut digits = vec![1,2,3,4,5,6,7,8,9];
     if n[0] > n[1] {
         digits.remove(n[0] as usize - 1);
@@ -44,7 +44,7 @@ fn three_digit_pair(n: [u32; 2]) -> Vec<[u32; 3]> {
         for b in digits.iter() {
             for c in digits.iter() {
                 if a != b && b != c && c != a{
-                    out.push([*a,*b,*c]);
+                    out.push(vec![*a,*b,*c]);
                 }
             }
         }
@@ -52,31 +52,51 @@ fn three_digit_pair(n: [u32; 2]) -> Vec<[u32; 3]> {
     return out;
 }
 
-fn int_to_digits(n: u64) -> [u32; 4] {
-    let mut digits = [0u32; 4];
+
+// Get all the 4-digit numbers that pair with a given 1-digit number
+fn four_digit_pair(n: u64) -> Vec<Vec<u64>> {
+    let mut digits = vec![1,2,3,4,5,6,7,8,9];
+    digits.remove(n as usize - 1);
+
+    let mut out = Vec::new();
+    for a in digits.iter() {
+        for b in digits.iter() {
+            for c in digits.iter() {
+                for d in digits.iter() {
+                    if a != b && b != c && c != a && d != a && d != b && d != c{
+                        out.push(vec![*a,*b,*c,*d]);
+                    }
+                }
+            }
+        }
+    }
+    return out;
+}
+
+fn int_to_digits(n: u64) -> Vec<u64> {
+    let mut digits = Vec::new();
     let mut num = n;
-    let mut ctr = 4;
     while num != 0 {
-        ctr -= 1;
         let q = num/10;
         let r = num%10;
-        digits[ctr] = r as u32;
+        digits.insert(0,r);
         num = q;
     }
     return digits;
 }
 
-pub fn euler32() -> u64 {
+fn equations() -> u64 {
     let mut out = 0;
     let two_digit = all_valid_two_digit();
     let mut prods = Vec::new();
 
     let ds = "[1, 2, 3, 4, 5, 6, 7, 8, 9]";
 
+    // Two digit times three digit
     for t in two_digit.iter() {
         let n = (10*t[0] + t[1]) as u64;
 
-        let complement = three_digit_pair(*t);
+        let complement = three_digit_pair(t.to_vec());
 
         for c in complement.iter() {
             let m = (100*c[0] + 10*c[1] + c[2]) as u64;
@@ -87,16 +107,9 @@ pub fn euler32() -> u64 {
             let p = int_to_digits(n*m);
 
             let mut v = Vec::new();
-
-            for i in t.iter() {
-                v.push(i);
-            }
-            for i in c.iter() {
-                v.push(i);
-            }
-            for i in p.iter() {
-                v.push(i);
-            }
+            v.extend(t);
+            v.extend(c);
+            v.extend(p);
 
             v.sort();
 
@@ -111,5 +124,40 @@ pub fn euler32() -> u64 {
 
         }
     }
+
+    // One digit times four digit
+    for n in 1..10 {
+        let complement = four_digit_pair(n);
+
+        for c in complement.iter() {
+            let m = (1000*c[0] + 100*c[1] + 10*c[2] + c[3]) as u64;
+
+            if n*m < 1000 || n*m > 9999 {
+                continue
+            }
+            let p = int_to_digits(n*m);
+
+            let mut v = vec![n];
+            v.extend(c);
+            v.extend(p);
+
+            v.sort();
+
+            let vs = format!("{:?}",v);
+            if vs == ds {
+                if !prods.contains(&(m*n)) {
+                    out += m*n;
+                    prods.push(m*n);
+                }
+                println!("{} * {} = {}", n,m,n*m);
+            }
+
+        }
+    }
+
     return out;
+}
+
+pub fn euler32() -> u64 {
+    return equations();
 }
