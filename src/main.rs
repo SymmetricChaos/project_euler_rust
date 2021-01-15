58988a2187dd73d3de901e217d960f22378a1c8f
SymmetricChaos <ajfraebel@gmail.com>
2021-01-15 17:54:39 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,63 +3,77 @@ use std::io;
 
 mod rationals;
 mod aux_funcs;
-
 mod worked_problems;
 
+fn make_message(input: &str) {
+    let out_string = format!("\nSee Problem At: https://projecteuler.net/problem={}",input);
+    let n = match input {
+        "1" => worked_problems::euler_1::euler1().to_string(),
+        "2" => worked_problems::euler_2::euler2().to_string(),
+        "3" => worked_problems::euler_3::euler3().to_string(),
+        "4" => worked_problems::euler_4::euler4().to_string(),
+        "5" => worked_problems::euler_5::euler5().to_string(),
+        "6" => worked_problems::euler_6::euler6().to_string(),
+        "7" => worked_problems::euler_7::euler7().to_string(),
+        "8" => worked_problems::euler_8::euler8().to_string(),
+        "9" => worked_problems::euler_9::euler9().to_string(),
+        "10" => worked_problems::euler_10::euler10().to_string(),
+        "11" => worked_problems::euler_11::euler11().to_string(),
+        "12" => worked_problems::euler_12::euler12().to_string(),
+        "13" => worked_problems::euler_13::euler13().to_string(),
+        "14" => worked_problems::euler_14::euler14().to_string(),
+        "15" => worked_problems::euler_15::euler15().to_string(),
+        "16" => worked_problems::euler_16::euler16().to_string(),
+        "17" => worked_problems::euler_17::euler17().to_string(),
+        "18" => worked_problems::euler_18::euler18().to_string(),
+        "19" => worked_problems::euler_19::euler19().to_string(),
+        "20" => worked_problems::euler_20::euler20().to_string(),
+        "21" => worked_problems::euler_21::euler21().to_string(),
+        "22" => worked_problems::euler_22::euler22().to_string(),
+        "23" => worked_problems::euler_23::euler23().to_string(),
+        "24" => worked_problems::euler_24::euler24().to_string(),
+        "25" => worked_problems::euler_25::euler25().to_string(),
+        "26" => worked_problems::euler_26::euler26().to_string(),
+        "27" => worked_problems::euler_27::euler27().to_string(),
+        "28" => worked_problems::euler_28::euler28().to_string(),
+        "29" => worked_problems::euler_29::euler29().to_string(),
+        "30" => worked_problems::euler_30::euler30().to_string(),
+        "31" => worked_problems::euler_31::euler31().to_string(),
+        "32" => worked_problems::euler_32::euler32().to_string(),
+        "33" => worked_problems::euler_33::euler33().to_string(),
+        "34" => worked_problems::euler_34::euler34().to_string(),
+        "35" => worked_problems::euler_35::euler35().to_string(),
+        "36" => worked_problems::euler_36::euler36().to_string(),
+        "37" => worked_problems::euler_37::euler37().to_string(),
+        "38" => worked_problems::euler_38::euler38().to_string(),
+        "39" => worked_problems::euler_39::euler39().to_string(),
+        "40" => worked_problems::euler_40::euler40().to_string(),
+        "41" => worked_problems::euler_41::euler41().to_string(),
+        _ => "Haven't gotten to that one yet, sorry.".to_string(),
+    };
+
+    println!("{}\nAnswer is: {}",out_string,n)
+}
 
 fn main() {
     println!("\nWelcome to My Project Euler Rust Project!");
-    println!("You may type \"quit\" to exit.");
+    println!("Type \"quit\" to exit.");
     loop {
-        println!("\n\nWhich Project Euler Problem Would You Like To See?");
+        println!("\n\nWhich Project Euler Problem Should I Calculate?");
         let mut val = String::new();
         io::stdin().read_line(&mut val).expect("Failed to read line");
 
-        match val.trim()  {
-            "1" =>  println!("\nEuler  1: {}",worked_problems::euler_1::euler1()),
-            "2" =>  println!("\nEuler  2: {}",worked_problems::euler_2::euler2()),
-            "3" =>  println!("\nEuler  3: {}",worked_problems::euler_3::euler3()),
-            "4" =>  println!("\nEuler  4: {}",worked_problems::euler_4::euler4()),
-            "5" =>  println!("\nEuler  5: {}",worked_problems::euler_5::euler5()),
-            "6" =>  println!("\nEuler  6: {}",worked_problems::euler_6::euler6()),
-            "7" =>  println!("\nEuler  7: {}",worked_problems::euler_7::euler7()),
-            "8" =>  println!("\nEuler  8: {}",worked_problems::euler_8::euler8()),
-            "9" =>  println!("\nEuler  9: {}",worked_problems::euler_9::euler9()),
-            "10" => println!("\nEuler 10: {}",worked_problems::euler_10::euler10()),
-            "11" => println!("\nEuler 11: {}",worked_problems::euler_11::euler11()),
-            "12" => println!("\nEuler 12: {}",worked_problems::euler_12::euler12()),
-            "13" => println!("\nEuler 13: {}",worked_problems::euler_13::euler13()),
-            "14" => println!("\nEuler 14: {}",worked_problems::euler_14::euler14()),
-            "15" => println!("\nEuler 15: {}",worked_problems::euler_15::euler15()),
-            "16" => println!("\nEuler 16: {}",worked_problems::euler_16::euler16()),
-            "17" => println!("\nEuler 17: {}",worked_problems::euler_17::euler17()),
-            "18" => println!("\nEuler 18: {}",worked_problems::euler_18::euler18()),
-            "19" => println!("\nEuler 19: {}",worked_problems::euler_19::euler19()),
-            "20" => println!("\nEuler 20: {}",worked_problems::euler_20::euler20()),
-            "21" => println!("\nEuler 21: {}",worked_problems::euler_21::euler21()),
-            "22" => println!("\nEuler 22: {}",worked_problems::euler_22::euler22()),
-            "23" => println!("\nEuler 23: {}",worked_problems::euler_23::euler23()),
-            "24" => println!("\nEuler 24: {}",worked_problems::euler_24::euler24()),
-            "25" => println!("\nEuler 25: {}",worked_problems::euler_25::euler25()),
-            "26" => println!("\nEuler 26: {}",worked_problems::euler_26::euler26()),
-            "27" => println!("\nEuler 27: {}",worked_problems::euler_27::euler27()),
-            "28" => println!("\nEuler 28: {}",worked_problems::euler_28::euler28()),
-            "29" => println!("\nEuler 29: {}",worked_problems::euler_29::euler29()),
-            "30" => println!("\nEuler 30: {}",worked_problems::euler_30::euler30()),
-            "31" => println!("\nEuler 31: {}",worked_problems::euler_31::euler31()),
-            "32" => println!("\nEuler 32: {}",worked_problems::euler_32::euler32()),
-            "33" => println!("\nEuler 33: {}",worked_problems::euler_33::euler33()),
-            "34" => println!("\nEuler 34: {}",worked_problems::euler_34::euler34()),
-            "35" => println!("\nEuler 35: {}",worked_problems::euler_35::euler35()),
-            "36" => println!("\nEuler 36: {}",worked_problems::euler_36::euler36()),
-            "37" => println!("\nEuler 37: {}",worked_problems::euler_37::euler37()),
-            "38" => println!("\nEuler 38: {}",worked_problems::euler_38::euler38()),
-            "39" => println!("\nEuler 39: {}",worked_problems::euler_39::euler39()),
-            "40" => println!("\nEuler 40: {}",worked_problems::euler_40::euler40()),
-            "41" => println!("\nEuler 41: {}",worked_problems::euler_41::euler41()),
-            "quit" => break,
-            "q" => break,
-            _ => println!("Haven't gotten to that one yet, sorry.")
+        let v = val.trim();
+            
+        if v == "q" || v == "quit" {
+            break
         }
+
+        if !v.chars().all(char::is_numeric) {
+            println!("\nERROR: Must input an integer or a valid command.");
+            continue
+        }
+
+        make_message(v);
     }
 }
