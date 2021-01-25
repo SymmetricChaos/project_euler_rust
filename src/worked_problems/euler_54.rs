90e80fa0696ce66cabe0051773a5f14fe9815843
SymmetricChaos <ajfraebel@gmail.com>
2021-01-25 08:54:59 -0500

    encoded card info

--- src/worked_problems/euler_54.rs
+++ src/worked_problems/euler_54.rs
@@ -4,31 +4,48 @@
 
 */
 
-use maplit::hashmap;
 use std::fs;
 
-// Return 1 for p1 wins, 2 for p2 wins, and 0 for a tie
-fn high_card_winner(p1: &Vec, p2: &Vec, rank: &Vec) -> u8 {
-     
+#[derive(Debug)]
+struct Card {
+    rank: u8,
+    suit: char,
+}
+
+fn str_to_card(s: &str) -> Card {
+    let v: Vec<char> = s.chars().collect();
+    let rank = match v[0] {
+        '2' => 2,
+        '3' => 3,
+        '4' => 4,
+        '5' => 5,
+        '6' => 6,
+        '7' => 7,
+        '8' => 8,
+        '9' => 9,
+        'T' => 10,
+        'J' => 11,
+        'Q' => 12,
+        'K' => 13,
+        'A' => 14,
+        _ => 100
+    };
+    Card{ rank: rank, suit: v[1]}
 }
 
 
+fn compare_hands(p1: &Vec<&str>, p2: &Vec<&str>) -> u8 {
+    let p1hand: Vec<Card> = p1.into_iter().map(|x| str_to_card(x)).collect();
+    let p2hand: Vec<Card> = p2.into_iter().map(|x| str_to_card(x)).collect();
+
+    println!("{:?}",p1hand);
+    println!("{:?}",p2hand);
+    0u8
+}
+
+
+
 pub fn euler54() -> u64 {
-    let rankmap = hashmap! {
-        "2" => 2,
-        "3" => 3,
-        "4" => 4,
-        "5" => 5,
-        "6" => 6,
-        "7" => 7,
-        "8" => 8,
-        "9" => 9,
-        "J" => 10,
-        "Q" => 11,
-        "K" => 12,
-        "A" => 13,
-    };
-    println!("{} -> {}","Q",rankmap["Q"]);
 
     let s = fs::read_to_string("Euler54Doc.txt").unwrap();
     let games: Vec<&str> = s.split("\r\n").collect();
@@ -41,7 +58,7 @@ pub fn euler54() -> u64 {
         p2_hands.push(g[15..29].split(" ").collect());
     }
 
-
+    compare_hands(&p1_hands[0],&p2_hands[0]);
 
     0u64
 }
