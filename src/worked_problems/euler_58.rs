a41384401d65214b0e028ea8a6bb83e621f7c093
SymmetricChaos <ajfraebel@gmail.com>
2021-01-31 08:11:23 -0500

    Update euler_58.rs

--- src/worked_problems/euler_58.rs
+++ src/worked_problems/euler_58.rs
@@ -32,9 +32,33 @@ pub fn euler58() -> u64 {
 
 pub fn euler58_example() {
     println!("\nProblem: What is the side length of the square spiral for which the ratio of primes along both diagonals first falls below 10%?");
-    println!("\n\n");
+    println!("\n\nAs in problem 28 there's no need to build the whole spiral in memory when the side lengths are so predictable.");
     let s = "
-";
+use crate::aux_funcs::{is_prime};
+
+pub fn euler58() -> u64 {
+
+    let mut step = 0;
+    let mut pos = 1;
+
+    let mut prime_ctr = 0;
+    let mut total_ctr = 1;
+
+    loop {
+        step += 2;
+        total_ctr += 4;
+        for _ in 0..4 {
+            pos += step;
+            if is_prime(pos) {
+                prime_ctr += 1
+            }
+        }
+        if prime_ctr*10 < total_ctr {
+            break
+        }
+    }
+    step+1
+}";
     println!("\n{}\n",s);
     println!("The answer is: {}",euler58());
 }
