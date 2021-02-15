b557c6c316b52215548788ad147027a3889f8adc
SymmetricChaos <ajfraebel@gmail.com>
2021-02-15 09:29:48 -0500

    euler_77
    got it by borrowing idea from 31

--- src/worked_problems/euler_77.rs
+++ src/worked_problems/euler_77.rs
@@ -2,43 +2,88 @@
 /*
 */
 
-use crate::aux_funcs::{is_prime32};
+use crate::aux_funcs::{prime_sieve};
+
+
+
+fn restricted_partition(coins: &Vec<u64>, pos: usize, cur: u64) -> u64 {
+    let mut total = 0;
+    for (n,c) in coins.iter().enumerate() {
+        if n < pos {
+            // Skip any coins smaller than the one currently being used
+            continue;
+        }
+        if *c > cur {
+            // Do nothing if the coin type is too big
+        } else if *c == cur {
+            // If we reach exactly zero add one to the total
+            total += 1;
+        } else {
+            // Otherwise continue to recur
+            total += restricted_partition(coins,n,cur-c);
+        }
+    }
+    total
+}
 
 pub fn euler77() -> u64 {
-    let mut ctr = 2;
-    let mut known_partitions = vec![0,0];
-    let mut primes = vec![];
+    let mut sieve = prime_sieve();
+    let mut primes = vec!{sieve.next().unwrap()};
+    let mut ctr = 10;
     loop {
-        let mut num = 0;
-        if is_prime32(ctr as u32) {
-            primes.push(ctr);
-            num += 1
-        }
-        for p in primes.iter() {
-            let diff = ctr-p;
-            if diff < *p {
-                break
-            }
-            if primes.contains(&diff) {
-                num += 1;
-            }
-            num += known_partitions[diff as usize]
-        }
-        println!("{}: {}",ctr,num);
-        if num > 5000 {
-            break ctr
-        }
-        known_partitions.push(num);
         ctr += 1;
+        while *primes.last().unwrap() < ctr {
+            primes.push(sieve.next().unwrap())
+        }
+        if restricted_partition(&primes,0,ctr) > 5000 {
+            break
+        }
     }
+    ctr
 }
 
 
 pub fn euler77_example() {
     println!("\nProblem: What is the first value which can be written as the sum of primes in over five thousand different ways?");
-    println!("\n\n");
+    println!("\n\nEssentially the same method that worked for Problem 31 also works here. We need only change the array to a vector and grow it as necessary for each new value.");
     let s = "
-";
+use crate::aux_funcs::{prime_sieve};
+
+fn restricted_partition(coins: &Vec<u64>, pos: usize, cur: u64) -> u64 {
+    let mut total = 0;
+    for (n,c) in coins.iter().enumerate() {
+        if n < pos {
+            // Skip any coins smaller than the one currently being used
+            continue;
+        }
+        if *c > cur {
+            // Do nothing if the coin type is too big
+        } else if *c == cur {
+            // If we reach exactly zero add one to the total
+            total += 1;
+        } else {
+            // Otherwise continue to recur
+            total += restricted_partition(coins,n,cur-c);
+        }
+    }
+    total
+}
+
+pub fn euler77() -> u64 {
+    let mut sieve = prime_sieve();
+    let mut primes = vec!{sieve.next().unwrap()};
+    let mut ctr = 10;
+    loop {
+        ctr += 1;
+        while *primes.last().unwrap() < ctr {
+            primes.push(sieve.next().unwrap())
+        }
+        if restricted_partition(&primes,0,ctr) > 5000 {
+            break
+        }
+    }
+    ctr
+}";
     println!("\n{}\n",s);
     println!("The answer is: {}",euler77());
 }
