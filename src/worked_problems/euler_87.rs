6b1f910d7ea714b20be231b9b016b7a7b9fd2947
SymmetricChaos <ajfraebel@gmail.com>
2021-02-25 12:14:18 -0500

    comments for 87

--- src/worked_problems/euler_87.rs
+++ src/worked_problems/euler_87.rs
@@ -67,9 +67,67 @@ pub fn euler87() -> u64 {
 
 pub fn euler87_example() {
     println!("\nProblem: How many numbers below fifty million can be expressed as the sum of a prime square, prime cube, and prime fourth power?");
-    println!("\n\n");
+    println!("\n\nThe first thing that should be obvious about this problem is that we can't do it by checking each number and seeing if it can be broken down this way, that's much too complicated to do fifty million times. Instead we'll create the number from the parts. To avoid doing extra work we can combine the 3rd and 4th powers into a single number these are all unique and there are only 1552 of them. Then we can just combine each of these numbers with each of the squares and check if the result is less than fifty millon. However these combinations are not all unique so we keep track of what we've seen before.");
     let s = "
-";
+use crate::aux_funcs::prime_sieve;
+use std::collections::HashSet;
+
+pub fn euler87() -> u64 {
+    let mut little = Vec::new();
+    let mut big = Vec::new();
+    let mut cube = Vec::new();
+    let mut quad = Vec::new();
+
+    let mut primes = prime_sieve();
+
+    // Get the squares, cubes, and 4th powers
+    loop {
+        let p = primes.next().unwrap();
+        if p*p >= 50_000_000 {
+            break
+        }
+        if p*p < 50_000_000 {
+            little.push(p*p)
+        }
+        if p*p*p < 50_000_000 {
+            cube.push(p*p*p)
+        }
+        if p*p*p*p < 50_000_000 {
+            quad.push(p*p*p*p)
+        }
+    }
+
+    // Merge the cubes and 4th powers
+    for c in cube {
+        for q in &quad {
+            if c+q < 50_000_000 {
+                big.push(c+q)
+            }
+        }
+    }
+
+    // Count
+    let mut known = HashSet::<u64>::new();
+    let mut out = 0;
+    for n in big {
+        let mut ctr = 0;
+        for m in &little {
+            let s = n+m;
+
+            if s > 50_000_000 {
+                break
+            } else {
+                if !known.contains(&s) {
+                    ctr += 1;
+                    known.insert(s);
+                }
+            }
+        }
+        out += ctr;
+    }
+
+    out as u64
+}";
     println!("\n{}\n",s);
     println!("The answer is: {}",euler87());
 }
