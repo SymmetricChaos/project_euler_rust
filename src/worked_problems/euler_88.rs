a011e5fc9fa836b67107deb8fd858250fbb4eabc
SymmetricChaos <ajfraebel@gmail.com>
2021-02-27 07:39:20 -0500

    semiworking euler88
    found a method for generating solutions

--- src/worked_problems/euler_88.rs
+++ src/worked_problems/euler_88.rs
@@ -8,21 +8,40 @@
 12 = 1*1*1*1*2*6 = 1+1+1+1+2+6
 
 The 1s are quite important here because they increase the sum but not the product.
-For an N-tuple we essentially need a factorization and then some 1s to fill in the rest.
+For an k-tuple we essentially need a factorization and then some 1s to fill in the rest.
+Primes can never qualify.
+It is not feasible to calculate every factorization for numbers unless we reach k = 12000.
+What if we try to build the factorization from the tuple?
 */
 
-fn prod_sum_num(n: usize) -> u64 {
-    let set = vec![1u64; n];
-    let s: u64 = set.iter().sum();
-    let p: u64 = set.iter().product();
-    println!("{:?}",set);
-    println!("{:?}",s);
-    println!("{:?}",p);
-    0u64
+use std::collections::HashSet;
+
+fn prod_sum_num(k: usize) -> u64 {
+    for i in 2.. {
+        let mut set = vec![1u64; k];
+        set[0] = i;
+        for pos in 1..k {
+            while set[pos] < set[pos-1] {
+                set[pos] += 1;
+                let s: u64 = set.iter().sum();
+                let p: u64 = set.iter().product();
+                if s == p {
+                    println!("{:?}",set);
+                    return s
+                }
+            }
+        }
+    }
+    unreachable!()
 }
 
 pub fn euler88() -> u64 {
-    prod_sum_num(4)
+    let mut nums = HashSet::new();
+    for k in 2..=6 {
+        nums.insert(prod_sum_num(k));
+    }
+    let out: u64 = nums.iter().sum();
+    out
 }
 
 pub fn euler88_example() {
