2b4c37c85a645c94fc553c0926806dd90df04951
SymmetricChaos <ajfraebel@gmail.com>
2021-04-09 12:15:23 -0400

    Update euler_91.rs

--- src/worked_problems/euler_91.rs
+++ src/worked_problems/euler_91.rs
@@ -1,20 +1,20 @@
 // Problem: How many right tirangles with integer coordinates including the origin can be made given that 0 ≤ x1, y1, x2, y2 ≤ 50?
 
 /*
-Many of these right triangles will have their right angle aligned with the grid. The real challenge is the other ones.
+A huge portion of the triangles can be found by hand. 
+Observe that if the right angle is at the origin then only x2 and y2 are free so there are 50*50 = 2500 triangles of that kind. 
+Likewise if the triangle lies on the x-axis or if it lies along the y-axis there are only two free variables. 
+There are thus 7500 of these trivial triangles.
 
-The aligned triangles should be straightforward to count using combinatorics.
-If the right angle is at the origin then x1=0 and y1=0 and there is one triangle for every permutation values of x2 and y2. In this case 50*50 = 2500
-If the right angle is on the x-axis but not the origin then y1=0 and x1=x2, so again there are 2500.
-The same argument applies if the right angle is on the y-axis but not the origin so 2500 more.
 
 Perhaps we can produce pythagorean triples and use them?
 We want the hypotenuse along the x or y-axis.
 
-Assume the hypotenuse lies along the x axis now using the right triangle altitude theorem we know that the following holds for height h the splits the hypotenuse into e and d
+Assume the hypotenuse lies along the x axis now using the right triangle altitude theorem we know that the following holds for height h that splits the hypotenuse into e and d
 h*h = e*d
-thus is e*d is a perfect square then h is an integer
+thus if e*d is a perfect square then h is an integer
 
+I am currently missing All THE triangle that do not lie along ANY axis
 */
 
 
@@ -24,15 +24,16 @@ pub fn euler91() -> u64 {
     let mut out = 7500;
     
     // Perfect squares with root less than or equal to 50
-    let squares: Vec<u32> = (1..50).map(|x| x*x).collect();
+    let squares: Vec<u32> = (1..=50).map(|x| x*x).collect();
+    println!{"{:?}",squares}
+
+    // Iterate over ordered pairs (a,b) and sum a+b <= 50
     for a in 1..=50 {
-        for b in a..=50-a {
+        for b in 1..=50-a {
+            // If a*b is in the set of squares then the heigher is an integer
             if squares.contains(&(a*b)) {
-                if a == b {
-                    out += 2
-                } else {
-                    out += 4
-                }
+                println!("{},{},{}",a,b,a*b);
+                out += 2
             }
         }
     }
@@ -42,7 +43,7 @@ pub fn euler91() -> u64 {
 
 pub fn euler91_example() {
     println!("\nProblem: How many right tirangles with integer coordinates including the origin can be made given that 0 ≤ x1, y1, x2, y2 ≤ 50?");
-    println!("\n\n");
+    println!("\n\nA huge portion of the triangles can be found by hand. Observe that if the right angle is at the origin then only x2 and y2 are free so there are 50*50 = 2500 triangles of that kind. Likewise if the triangle lies on the x-axis or if it lies along the y-axis there are only two free variables. There are thus 7500 of these trivial triangles.");
     let s = "
 ";
     println!("\n{}\n",s);
@@ -52,6 +53,6 @@ pub fn euler91_example() {
 /*
 #[test]
 fn test91() {
-    assert_eq!(euler91(),)
+    assert_eq!(euler91(),14234 )
 }
 */
